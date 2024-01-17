9958fb6f6a579b06a2629349e3cfe24c46d6f891
kusrohit <kusrohit@users.noreply.github.com>
2024-01-17 09:59:30 +0530

    Addition program added

---
+++ PythonProgram/py2.py
@@ -0,0 +1,7 @@
+# adding the number when user input
+
+num_1 = int(input('Enter the first number : '))
+num_2 = int(input('Enter the second number : '))
+
+add = num_1 + num_2
+print(f'Addition of {num_1} and {num_2} is {add}')
